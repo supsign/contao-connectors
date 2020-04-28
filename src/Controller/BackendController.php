b7a732b5f741ac3cd8a09a7799dfd49eb52cd0ca
Florian Ratz <florian.ratz@supsign.ch>
2020-04-28 13:00:15 +0200

    ...

--- src/Controller/BackendController.php
+++ src/Controller/BackendController.php
@@ -5,43 +5,25 @@ namespace Supsign\ContaoConnectorsBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Contao\MemberModel;
-use Contao\MemberGroupModel;
 
 /**
- * @Route("/contao/test-route",
- *     name=BackendController::class,
- *     defaults={
- *         "_scope" = "backend",
- *         "_token_check" = true,
- *         "_backend_module" = "connectors-bundle"
- *     }
- * )
+ * @Route("/contao", defaults={
+ *     "_scope" = "backend",
+ *     "_token_check" = false,
+ *     "_backend_module" = "contao-connectors"
+ * })
  */
 class BackendController extends AbstractController
 {
-    private $twig;
-    
-    public function __construct(TwigEnvironment $twig)
-    {
-        $this->twig = $twig;
-    }
-
-    public function __invoke()
-    {
-        return new Response(
-            $this->get('twig')->render('@ContaoConnectorsBundle/default.html.twig', [])
-        );
-    }
 
     /**
-     * @Route("/connectors-bundle", name="supsign.test")
+     * @Route("/connectors", name="supsign.test")
      */
 
     public function default()
     {
         return new Response(
-            this->get('twig')->render('@ContaoConnectorsBundle/default.html.twig', [])
+            $this->get('twig')->render('@ContaoConnectorsBundle/default.html.twig', [])
         );
-    } 
+    }
 }

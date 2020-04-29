2568850fb7c9727a151a4c59cee70c7f467a3aeb
Florian Ratz <florian.ratz@supsign.ch>
2020-04-29 12:35:14 +0200

    added config

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+use Supsign\ContaoConnectorsBundle\Model\FtpData;
+use Supsign\ContaoConnectorsBundle\Model\FtpProtocols;
+
+$GLOBALS['TL_MODELS']['tl_ftp_data'] = FtpData::class;
+$GLOBALS['TL_MODELS']['tl_ftp_protocls'] = FtpProtocols::class;

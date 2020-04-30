a62ee4e3807102ee445dcf33ba80f2480dff43fe
Florian Ratz <florian.ratz@supsign.ch>
2020-04-30 12:38:14 +0200

    no message

--- src/Controller/BackendController.php
+++ src/Controller/BackendController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Supsign\ContaoConnectorsBundle\Model\FtpDataModel;
 use Supsign\ContaoConnectorsBundle\Model\FtpProtocolsModel;
-Supsign\ContaoConnectorsBundle\Entity\FtpData;
+use Supsign\ContaoConnectorsBundle\Entity\FtpData;
 
 use Supsign\ContaoConnectorsBundle\TestClass;
 

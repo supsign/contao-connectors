64e952b73621ac2561e41304a2cadd0e9b0b408d
Florian Ratz <florian.ratz@supsign.ch>
2020-05-05 09:40:36 +0200

    table rename - config

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -4,4 +4,4 @@ use Supsign\ContaoConnectorsBundle\Model\FtpDataModel;
 use Supsign\ContaoConnectorsBundle\Model\FtpProtocolsModel;
 
 $GLOBALS['TL_MODELS']['tl_ftp_data'] = FtpDataModel::class;
-$GLOBALS['TL_MODELS']['tl_ftp_protocls'] = FtpProtocolsModel::class;
+$GLOBALS['TL_MODELS']['tl_ftp_protocols'] = FtpProtocolsModel::class;

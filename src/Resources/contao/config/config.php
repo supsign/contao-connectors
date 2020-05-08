9d52d96ae2f5fc10f2ee1c35eeb536551bce81db
Fabrizio Cocco <fabrizio.cocco@supsign.ch>
2020-05-08 07:33:29 +0200

    no message

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -7,3 +7,8 @@ use Supsign\ContaoConnectorsBundle\Model\FtpSyncConfigsModel;
 $GLOBALS['TL_MODELS']['tl_ftp_data'] = FtpDataModel::class;
 $GLOBALS['TL_MODELS']['tl_ftp_protocols'] = FtpProtocolsModel::class;
 $GLOBALS['TL_MODELS']['tl_ftp_sync_configs'] = FtpSyncConfigsModel::class;
+
+
+if (TL_MODE == 'BE') {
+    $GLOBALS['TL_CSS'][] = '/bundles/contaoconnectors/css/supsign-connectors.css';
+}
